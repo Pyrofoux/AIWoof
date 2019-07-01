1ec8eab4e24b338285519c2c5334ead69eb59da4
Younès Rabii <rabiiyounes@outlook.com>
2019-07-01 22:00:20 +0200

    Killer Queen V.0 - check History.txt

--- FoxuFoxu/FoxuFoxu/profileManager.py
+++
@@ -1,9 +0,0 @@
-
-
-def createProfiles(baseInfo,gameSetting):
-
-    #gameSetting
-    #{"enableNoAttack": false, "enableNoExecution": false, "enableRoleRequest": true, "maxAttackRevote": 1, "maxRevote": 1, "maxSkip": 2, "maxTalk": 10, "maxTalkTurn": 20, "maxWhisper": 10, "maxWhisperTurn": 20, "playerNum": 5, "randomSeed": 8528942025861373791, "roleNumMap": {"BODYGUARD": 0, "MEDIUM": 0, "POSSESSED": 1, "SEER": 1, "FOX": 0, "FREEMASON": 0, "VILLAGER": 2, "ANY": 0, "WEREWOLF": 1}, "talkOnFirstDay": false, "timeLimit": 1000, "validateUtterance": true, "votableInFirstDay": false, "voteVisible": true, "whisperBeforeRevote": false}
-
-
-    return None
---
+++ agents/FoxuFoxu/textMetrics.py
@@ -0,0 +1,92 @@
+from toolbox import *
+
+def updateTextMetrics(tracker):
+    #Evaluate behavioural metrics based on the way agent talk
+
+    for id in tracker.profiles:
+
+        profile = tracker.profiles[id]
+
+        hostilityPatterns = generateHostilityPatterns(tracker)
+        profile["hostility"]  = calculateHostility(profile, hostilityPatterns)
+        profile["complexity"] = calculateComplexity(profile)
+        #profile[""]
+
+
+
+def calculateHostility(profile, patterns):
+    #Evaluates hostility towards our agent
+    #... by recognizing hostile or protectives patterns
+
+    if profile['isMe'] :
+        return -100
+    else:
+
+        hostility = 0
+        talks = profile['talkHistory']
+
+        for talk in talks:
+            for pattern in patterns:
+
+                text = talk['text']
+                day = int(talk['day'])
+
+                occurences = text.count(pattern)
+                weight     = patterns[pattern]
+                hostility += occurences*weight*day
+
+        return hostility
+
+def calculateComplexity(profile):
+    #Evaluates the complexity of agent thoughts
+    #... by counting the number of brackets used :D
+
+    texts = getAllTexts(profile)
+    maxComplexity = 0
+    for text in texts:
+
+        score = text.count("(")
+        maxComplexity = max(score, maxComplexity)
+
+    return maxComplexity
+
+
+def generateHostilityPatterns(tracker):
+
+    myName = formatAgentName(tracker.myId)
+
+    patterns = {}
+
+
+    patterns['ESTIMATE '+myName+' WEREWOLF'] = 10
+    patterns['COMINGOUT '+myName+' WEREWOLF'] = 10
+    patterns['DIVINED '+myName+' WEREWOLF'] = 15
+
+    patterns['ESTIMATE '+myName+' POSSESSED'] = 10
+    patterns['COMINGOUT '+myName+' POSSESSED'] = 10
+    patterns['DIVINED '+myName+' POSSESSED'] = 15
+
+    patterns['ESTIMATE '+myName+' VILLAGER'] = -10
+    patterns['COMINGOUT '+myName+' VILLAGER'] = -10
+    patterns['DIVINED '+myName+' VILLAGER'] = -15
+
+    patterns['ESTIMATE '+myName+' HUMAN'] = -10
+    patterns['COMINGOUT '+myName+' HUMAN'] = -10
+    patterns['DIVINED '+myName+' HUMAN'] = -15
+
+    patterns['ESTIMATE '+myName+' MEDIUM'] = -10
+    patterns['COMINGOUT '+myName+' MEDIUM'] = -10
+    patterns['DIVINED '+myName+' MEDIUM'] = -15
+
+    patterns['VOTE '+myName] = 20
+
+    return patterns
+
+
+def getAllTexts(profile):
+
+    txts = []
+    for talk in profile['talkHistory']:
+        txts.append(talk["text"])
+
+    return txts
